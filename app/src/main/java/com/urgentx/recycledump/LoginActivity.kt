5c57e63cfc9d9b1a39f7183e078e2a10f2c63fb2
Barco <barcozaleta@gmail.com>
2017-06-14 22:47:39 +1200

    Add login activity.

---
+++ app/src/main/java/com/urgentx/recycledump/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.urgentx.recycledump
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginButton.setOnClickListener({
+            startActivity(Intent(this, MainActivity::class.java))
+        })
+    }
+}
