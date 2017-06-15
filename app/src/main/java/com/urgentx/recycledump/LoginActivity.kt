55904dc56041f10040733d4b4f7509abfb05ba52
Barco <barcozaleta@gmail.com>
2017-06-15 22:26:24 +1200

    Implement google sign in.

--- app/src/main/java/com/urgentx/recycledump/LoginActivity.kt
+++ app/src/main/java/com/urgentx/recycledump/LoginActivity.kt
@@ -1,18 +1,59 @@
 package com.urgentx.recycledump
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+    val RC_SIGN_IN = 12345
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        loginButton.setOnClickListener({
-            startActivity(Intent(this, MainActivity::class.java))
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("758826616739-vt7dh4p2s58180lhbu41vuc9382j10s5.apps.googleusercontent.com")
+                .requestEmail()
+                .build()
+
+        var mGoogleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
+
+        sign_in_button.setOnClickListener({
+            val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+            startActivityForResult(signInIntent, RC_SIGN_IN)
         })
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            handleSignInResult(result)
+        }
+    }
+
+    private fun handleSignInResult(result: GoogleSignInResult) {
+        if (result.isSuccess) {
+            // Signed in successfully, show authenticated UI.
+            //var acct = result.signInAccount;
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            // Signed out, show unauthenticated UI.
+        }
     }
 }
